1274b6a5504c50532308989225dc7dfde9f3f7e0
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-11 23:24:38 +0300

    remove unused imports

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import profileForm,UserUpdateForm,RegistrationForm,projectForm,UpdateUserProfileForm,RateForm
@@ -7,7 +6,6 @@ from .models import Projects,Profile
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.urls import reverse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Profile,Projects,Revieww
@@ -28,7 +26,6 @@ def register(request):
             profile.user=user
             profile.save()
 
-            # messages.success(request, f'Successfully created Account!.You can now login as {username}!')
         return redirect('log_user')
     else:
         form= RegistrationForm()
@@ -43,15 +40,15 @@ def searchprofile(request):
     if 'searchUser' in request.GET and request.GET['searchUser']:
         name = request.GET.get("searchUser")
         searchResults = Projects.search_projects(name)
-        message = f'name'
+       
         params = {
             'results': searchResults,
-            'message': message
+            
         }
         return render(request, 'search.html', params)
-    else:
-        message = "You haven't searched for any profile"
-    return render(request, 'search.html', {'message': message})
+    # else:
+    #     message = "You haven't searched for any profile"
+    return render(request, 'search.html')
 
 @login_required(login_url='log_user')   
 def addProject(request):
