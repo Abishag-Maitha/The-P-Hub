6461b421552b27f628b904d2a9c3e56affc41db8
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-12 21:31:56 +0300

    remove auto imports

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import profileForm,UserUpdateForm,RegistrationForm,projectForm,UpdateUserProfileForm,RateForm
