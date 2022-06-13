8a0affa6eebb9719213681c15831335452cb1cf7
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-06-13 18:45:59 +0300

    add api url names

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     path('',views.index, name = 'home'),
     path('register/',views.register, name='registration'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='log_user'),
-    path('search/', views.searchprofile, name='search'),
+    path('search/', views.searchproject, name='search'),
     path('newproject/',views.addProject,name = 'project'),
     path('profile/<id>/',views.profile,name = 'profile'),
     path('editprofile/',views.editprofile,name = 'editprofile'), 
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
-    path('api/profile',views.ProfileList.as_view()),
-    path('api/projects',views.ProjectList.as_view()),
-    path('projects/<id>/',views.projects,name = 'projects'),
+    path('api/profile',views.ProfileList.as_view(), name='apiprofiles'),
+    path('api/projects',views.ProjectList.as_view(), name='apiprojects'),
+    path('projects/<int:id>',views.projects,name = 'projects'),
     path(r'ratings/', include('star_ratings.urls', namespace='ratings')),
     path('rate/<id>/',views.rate,name = 'rate')
 
